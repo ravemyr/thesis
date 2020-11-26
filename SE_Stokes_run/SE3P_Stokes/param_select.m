a3a63248de98dfdca128f61845b43e27e436a65a
ravemyr <ravemyr@kth.se>
2020-11-26 10:56:48 +0100

    param changes

--- SE_Stokes_run/SE3P_Stokes/param_select.m
+++ SE_Stokes_run/SE3P_Stokes/param_select.m
@@ -13,10 +13,12 @@ box = [L L L]; % periodic box
 opt.box = box;
 
 %Ewald params
-M0 = 16; % Set M0=M/L, the restu * 1+ is automatic
+
+
+M0 = 128; % Set M0=M/L, the restu * 1+ is automatic
 opt.M = M0*opt.box;
 opt.xi = M0*pi/12;
-opt.P = 16;
+opt.P = opt.M/2;
 opt.betaP = 2.5;
 
 % charge-neutral system
@@ -34,13 +36,15 @@ ref = SE3P_Stokes_direct_fd_mex(1:N,x,f,ED_opt);
 
 F = sum(abs(f).^2);
 est = @(M,xi,L,F) 2.*L^2.*sqrt(F).*(2*sqrt(pi)*M/2 + 3*xi*L).*exp(-((M/2)*pi/(xi*L)).^2)/sqrt(pi);
-MM = [16,24,32,40,48,56,64];
+MM = [16,32,64];
 err = [];
 ee = [];
 for i = MM
     M0 = i;
     opt.M = M0*opt.box;
-    u = SE3P_Stokes(1:N, x, f, opt);
+    ED_opt.layers = (opt.M(1)-1)/2;
+    %u = SE3P_Stokes(1:N, x, f, opt);
+    u = SE3P_Stokes_direct_fd_mex(1:N,x,f,ED_opt);
     err = [err rms(u-ref) / rms(ref)];
     ee = [ee, est(opt.M, opt.xi,L,F)];
 end
@@ -50,30 +54,31 @@ disp('error estimate')
 disp(ee)
 semilogy(MM./2,err)
 exportgraphics(gcf,'error_kplot.png')
-semilogy(MM./2,ee)
-exportgraphics(gcf,'error_est.png')
+%semilogy(MM./2,ee)
+%exportgraphics(gcf,'error_est.png')
 %% Amplitute
 
-A = @(F,xi,L) (F*xi/L)^(1/2); %Hypothesis
+A = @(F,xi,L) (F*xi/L).^(1/2); %Hypothesis
 
-M0 = 64; 
-opt.P =32;
+M0 = 128;
+opt.M = [M0,M0,M0];
+opt.P =48;
 opt.window = 'kaiser_exact';
-NN = [10,50,100,200,400];
-LL = [0.5, 1, 2, 5, 10];
-xx = [2,4,6,8,10,12];
+NN = [25,50,100];
+LL = [0.5, 1, 2];
+xx = [4,6,8];
 MA = [];
 for L = LL
     opt.box = [L,L,L];
-    opt.M = M0*opt.box;
+    
     for N = NN
-        [x,f] = SE_charged_system(N,box,'vector');
+        [x,f] = SE_charged_system(N,opt.box,'vector');
         F = sqrt(sum(abs(f).^2));
         for xi = xx
             opt.xi = xi;
-            u = SE3P_Stokes(1:N, x, f, opt);
-            a = A(F,xi,L);
-            MA = [MA norm(u)/a];
+            u = norm(SE3P_Stokes(1:N, x, f, opt));
+            a = norm(A(F,xi,L));
+            MA = [MA u/a];
         end
     end
 end
