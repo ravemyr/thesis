4ed0b3363b2f971cc0ce96b2cdc45f3ad8abb681
ravemyr <ravemyr@kth.se>
2020-12-01 12:35:21 +0100

    new data

--- SE_Stokes_run/SE3P_Stokes/amp_param.m
+++ SE_Stokes_run/SE3P_Stokes/amp_param.m
@@ -1,14 +1,14 @@
 %% Amplitute
-
-A = @(F,xi,L) (F*xi^2/L).^(1/2); %Hypothesis
+rng(1);
+A = @(F,xi,L) (F*xi/(L)).^(1/2); %Hypothesis
 
 M0 = 128;
 opt.M = [M0,M0,M0];
 opt.P = 96;
 opt.window = 'gaussian';
-NN = [25,50,100];
-LL = [1,2,3];
-xx = [4,6,8];
+NN = [25,50,75,100];
+LL = [0.5,1,1.5,2];
+xx = [2,4,6,8,10,12];
 MA = [];
 fileid = fopen('outdata.txt','w');
 fprintf(fileid,'%2s %3s %4s %9s %11s %5s \n','N','L','xi', 'rms','A','rms/a');
@@ -21,12 +21,12 @@ for L = LL
             opt.xi = xi;
             eu = rms(SE3P_Stokes(1:N, x, f, opt));
             a = A(F,xi,L);
-            fprintf(fileid,'%3i %2.2f %1i %6.4f %6.4f %6.4f %6.4f %6.4f \n',N,L,xi,eu,norm(a),eu(2)/norm(a));
-            MA = [MA eu/norm(a)];
+            fprintf(fileid,'%3i %2.2f %1i %6.4f %6.4f %6.4f %6.4f %6.4f \n',N,L,xi,eu,norm(a),eu/a);
+            MA = [MA eu/a];
         end
     end
 end
 plot(1:length(MA),MA,'*')
 exportgraphics(gcf,'error_amp.png');
 fclose(fileid);
-disp(MA)
+%disp(MA)
