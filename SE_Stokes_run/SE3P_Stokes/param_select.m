d43cf342585c206a4d9a20e0ddc47b12b0980623
ravemyr <33254565+ravemyr@users.noreply.github.com>
2020-12-01 09:14:00 +0100

    splitting files for speed

--- SE_Stokes_run/SE3P_Stokes/param_select.m
+++ SE_Stokes_run/SE3P_Stokes/param_select.m
@@ -59,38 +59,6 @@ semilogy(MM./2,err([1,4,7]))
 exportgraphics(gcf,'error_kplot.png')
 %semilogy(MM./2,ee)
 %exportgraphics(gcf,'error_est.png')
-%% Amplitute
-
-A = @(F,xi,L) (F.^(1/2)*xi^2/(L^(1/2))); %Hypothesis
-
-M0 = 128;
-opt.M = [M0,M0,M0];
-opt.P =48;
-opt.window = 'kaiser_exact';
-NN = [25,50,100];
-LL = [0.5, 1, 2];
-xx = [4,6,8];
-MA = [];
-fileid = fopen('outdata.txt','w');
-fprintf(fileid,'%2s %3s %4s %9s %11s %5s \n','N','L','xi', 'rms','A','rms/a');
-for L = LL
-    opt.box = [L,L,L];
-    for N = NN
-        [x,f] = SE_charged_system(N,opt.box,'vector');
-        F = sqrt(sum(abs(f).^2));
-        for xi = xx
-            opt.xi = xi;
-            eu = rms(SE3P_Stokes(1:N, x, f, opt));
-            a = A(F,xi,L);
-            fprintf(fileid,'%3i %2.2f %1i %6.4f %6.4f %6.4f %6.4f %6.4f \n',N,L,xi,eu,norm(a),eu(2)/norm(a));
-            MA = [MA eu/norm(a)];
-        end
-    end
-end
-plot(1:length(MA),MA,'*')
-exportgraphics(gcf,'error_amp.png');
-fclose(fileid);
-disp(MA)
 
 
 
