5c2e910ff437b5e66bdc0d3d61725cd9681979d7
ravemyr <ravemyr@kth.se>
2020-12-01 13:32:54 +0100

    fix to error test

--- SE_Stokes_run/SE3P_Stokes/param_select.m
+++ SE_Stokes_run/SE3P_Stokes/param_select.m
@@ -48,14 +48,15 @@ for i = MM
     ED_opt.layers = (opt.M(1)-1)/2;
     %u = SE3P_Stokes(1:N, x, f, opt);
     u = SE3P_Stokes_direct_fd_mex(1:N,x,f,ED_opt);
-    err = [err rms(u-ref)];
-    ee = [ee, est(opt.M, opt.xi,L,F)];
+    err = [err rms(u-ref)/rms(ref)];
+    esti = est(opt.M,opt.xi,L,F);
+    ee = [ee, esti/rms(ref)];
 end
 disp('error rate as M increases')
 disp(err)
-disp('error estimate')
+disp('estimate error comparison')
 disp(ee)
-semilogy(MM./2,err([1,4,7]))
+semilogy(MM./2,err)
 exportgraphics(gcf,'error_kplot.png')
 %semilogy(MM./2,ee)
 %exportgraphics(gcf,'error_est.png')
