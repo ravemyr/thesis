ff1187eded9b5102ef40d8237bd90151a40029ab
ravemyr <ravemyr@kth.se>
2020-12-09 15:45:26 +0100

    new try

--- SE_Stokes_run/SE3P_Stokes/param_select.m
+++ SE_Stokes_run/SE3P_Stokes/param_select.m
@@ -50,10 +50,11 @@ xx = (20:4:24)*pi;
     est = @(M,xi,L,f) 2*L^2*sqrt(f)*(2*sqrt(pi)*M/2 + 3*xi*L)*exp(-((M/2)*pi/(xi*L))^2)/sqrt(pi);
     MM = [50,64,80,96,112,128,146];
 str = {};
+
 for xi = xx
     err = [];
     ee = [];
-    ref = refv(xx==xi);
+    ref = refv(:,xx==xi);
     for i = MM
         M0 = i;
         opt.M = M0*opt.box;
@@ -67,6 +68,7 @@ for xi = xx
         ee = [ee, esti];
     end
 semilogy(MM./2,err,'.-')
+disp(err)
 hold on
 semilogy(MM./2,ee,'.--')
 str = [str,strcat('computed error, \xi =', num2str(xi))];
