a1e041c6bbcd9cada956219c6305ee4043917d87
ravemyr <ravemyr@kth.se>
2020-12-10 13:00:36 +0100

    figs

--- SE_Stokes_run/SE3P_Stokes/param_select.m
+++ SE_Stokes_run/SE3P_Stokes/param_select.m
@@ -99,7 +99,7 @@ str = [str,strcat('computed error, \xi =', num2str(xi))];
 str = [str, strcat('error estimate, \xi =', num2str(xi))];
 str = [str, strcat('second estimate, \xi =', num2str(xi))];
 end
-axis([1,MM(end)/2,10^-15,100])
+axis([0,MM(end)/2,10^-15,1])
 legend(str{:},'Location','North East')
 
 
