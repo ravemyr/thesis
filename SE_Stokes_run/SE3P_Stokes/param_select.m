1a95ac220e36012e9fc7b792753e9836a1bc6858
ravemyr <33254565+ravemyr@users.noreply.github.com>
2020-12-10 13:26:34 +0100

    fixed reference generator

--- SE_Stokes_run/SE3P_Stokes/param_select.m
+++ SE_Stokes_run/SE3P_Stokes/param_select.m
@@ -31,49 +31,52 @@ rms_ev = [];
 xx = (8:4:16)*pi;
 
     %% Direct solution
-    if(exist('refval.mat'))
-            reffile = load('refval.mat');
-	    if(size(xx)==size(reffile.xx))
-	    	if(xx==reffile.xx)
-			disp('Using existing reference solution')
-			refv = reffile.refv;
-			rms_ev = reffile.rms_ev;
-	    	else
-                	disp('Generating reference solution')
-                	for xi = xx
-                    	ED_opt.xi = xi;
-		    	u = SE3P_Stokes_direct_fd_mex(1:N,x,f,ED_opt);
-		    	rms_e = rmse(u);
-		    	rms_ev =[rms_ev, rms_e];
-                    	refv = [refv,u];
-                    	save('refval.mat','refv','xx','rms_ev');
-                	end
-	    	end
-	     else
-		disp('Generating reference solution')
-		for xi = xx
-			ed_opt.xi = xi;
-			u =
-	    
-             else
-                disp('Generating reference solution')
-                for xi = xx
-       	        ED_opt.xi = xi;
-		u = SE3P_Stokes_direct_fd_mex(1:N,x,f,ED_opt);
-		rms_e = rmse(u);
-	    	rms_ev = [rms_ev,rms_e];
-		refv = [refv,u];
-		save('refval.mat','refv','xx','rms_ev');
+if(exist('refval.mat'))
+        reffile = load('refval.mat');
+    if(size(xx)==size(reffile.xx))
+        if(xx==reffile.xx)
+            disp('Using existing reference solution')
+            refv = reffile.refv;
+            rms_ev = reffile.rms_ev;
+        else
+            disp('Generating reference solution')
+            for xi = xx
+                ED_opt.xi = xi;
+                u = SE3P_Stokes_direct_fd_mex(1:N,x,f,ED_opt);
+                rms_e = rmse(u);
+                rms_ev =[rms_ev, rms_e];
+                refv = [refv,u];
+                save('refval.mat','refv','xx','rms_ev');
+            end
+        end
+    else
+        disp('Generating reference solution')
+        for xi = xx
+            ED_opt.xi = xi;
+            u = SE3P_Stokes_direct_fd_mex(1:N,x,f,ED_opt);
+            rms_e = rmse(u);
+            rms_ev =[rms_ev, rms_e];
+            refv = [refv,u];
+            save('refval.mat','refv','xx','rms_ev');
         end
     end
-    %opt.window = 'gaussian';
-    %ref = SE3P_Stokes(1:N,x,f,opt);
+else
+        disp('Generating reference solution')
+        for xi = xx
+            ED_opt.xi = xi;
+            u = SE3P_Stokes_direct_fd_mex(1:N,x,f,ED_opt);
+            rms_e = rmse(u);
+            rms_ev = [rms_ev,rms_e];
+            refv = [refv,u];
+            save('refval.mat','refv','xx','rms_ev');
+        end
+end
     %% Estimate
 
-    F = sum(norm(f).^2);
-    est = @(M,xi,L,f) 2*L^2*sqrt(f)*(2*sqrt(pi)*M/2 + 3*xi*L)*exp(-((M/2)*pi/(xi*L))^2)/sqrt(pi);
-    est2 = @(M,xi,L,f) sqrt(f)*(xi^3*L^2/(pi^4*(M/2)^(3/2)))*exp(-(pi*(M/2)/(xi*L))^2);
-    MM = [50,64,80,96,112,128,146];
+F = sum(norm(f).^2);
+est = @(M,xi,L,f) 2*L^2*sqrt(f)*(2*sqrt(pi)*M/2 + 3*xi*L)*exp(-((M/2)*pi/(xi*L))^2)/sqrt(pi);
+est2 = @(M,xi,L,f) sqrt(f)*(xi^3*L^2/(pi^4*(M/2)^(3/2)))*exp(-(pi*(M/2)/(xi*L))^2);
+MM = [50,64,80,96,112,128,146];
 str = {};
 
 for xi = xx
@@ -89,23 +92,22 @@ for xi = xx
         opt.P = M0/2;
         ED_opt.layers = (opt.M(1)-1)/2;
         ED_opt.xi = xi;
-	%u = SE3P_Stokes(1:N, x, f, opt);
+        %u = SE3P_Stokes(1:N, x, f, opt);
         u = SE3P_Stokes_direct_fd_mex(1:N,x,f,ED_opt);
         err = [err rmse(u-ref)/rmse(ref)];
         esti = est(opt.M(1),xi,L,F)/rms_e;
         ee = [ee, esti];
-	esti2 = est2(opt.M(1),xi,L,F)/rms_e;
-	eee = [eee esti2];
+        esti2 = est2(opt.M(1),xi,L,F)/rms_e;
+        eee = [eee esti2];
     end
-semilogy(MM./2,err,'.-')
-hold on
-semilogy(MM./2,ee,'.--')
-semilogy(MM./2,eee,':*')
-str = [str,strcat('computed error, \xi =', num2str(xi))];
-str = [str, strcat('error estimate, \xi =', num2str(xi))];
-str = [str, strcat('second estimate, \xi =', num2str(xi))];
+    semilogy(MM./2,err,'.-')
+    hold on
+    semilogy(MM./2,ee,'.--')
+    semilogy(MM./2,eee,':*')
+    str = [str,strcat('computed error, \xi =', num2str(xi))];
+    str = [str, strcat('error estimate, \xi =', num2str(xi))];
+    str = [str, strcat('second estimate, \xi =', num2str(xi))];
 end
-%axis([0,MM(end)/2,10^-15,1])
 ylim([10^-14,1])
 legend(str{:},'Location','North East')
 
