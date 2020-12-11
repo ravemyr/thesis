3c6adc30514f5036fd14276c8d8688d5ce08897a
ravemyr <ravemyr@kth.se>
2020-12-11 16:20:55 +0100

    imgs

--- SE_Stokes_run/SE3P_Stokes/P_error.m
+++ SE_Stokes_run/SE3P_Stokes/P_error.m
@@ -15,7 +15,7 @@ opt.box = box;
 %Ewald params
 
 
-M0 = 192; % Set M0=M/L, the restu * 1+ is automatic
+M0 = 128; % Set M0=M/L, the restu * 1+ is automatic
 opt.M = M0*[1,1,1];
 opt.xi = 30;
 opt.betaP = 2.5;
@@ -38,13 +38,13 @@ end
 	%opt.window = 'gaussian';
 %ref = SE3P_Stokes(1:N,x,f,opt);
 %% Compare solutions with changing P
-MM = 0.2:0.7:1.6;
+MM = 3:2:7;
 for m = MM
 	PP = 4:2:36;
 	rms_err = [];
 	for P = PP
     		opt.P = P;
-		opt.m = m*sqrt(P);
+		opt.m = m;
     		u = SE3P_Stokes(1:N, x, f, opt);
     		rms_err = [rms_err rmse(u-ref)/rmse(ref)];
 	end
