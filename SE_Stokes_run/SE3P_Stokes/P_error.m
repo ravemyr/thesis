8d2462526d22c68c6c56d044d12a74ed2edf4e65
ravemyr <ravemyr@kth.se>
2020-12-11 16:04:24 +0100

    imgs

--- SE_Stokes_run/SE3P_Stokes/P_error.m
+++ SE_Stokes_run/SE3P_Stokes/P_error.m
@@ -17,7 +17,7 @@ opt.box = box;
 
 M0 = 192; % Set M0=M/L, the restu * 1+ is automatic
 opt.M = M0*[1,1,1];
-opt.xi = 30;
+opt.xi = M0*pi/12;
 opt.betaP = 2.5;
 
 % charge-neutral system
@@ -38,13 +38,13 @@ end
 	%opt.window = 'gaussian';
 %ref = SE3P_Stokes(1:N,x,f,opt);
 %% Compare solutions with changing P
-MM = 48:16:128;
-for M = MM
-	opt.M = opt.box*M;
+MM = 0.2:0.7:1.6;
+for m = MM
 	PP = 4:2:36;
 	rms_err = [];
 	for P = PP
     		opt.P = P;
+		opt.m = m*sqrt(P);
     		u = SE3P_Stokes(1:N, x, f, opt);
     		rms_err = [rms_err rmse(u-ref)/rmse(ref)];
 	end
