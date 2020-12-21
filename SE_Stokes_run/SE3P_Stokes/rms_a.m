6300b30ed3a3544bdda84853eafa364fd0daef07
ravemyr <ravemyr@kth.se>
2020-12-21 10:05:16 +0100

    img

--- SE_Stokes_run/SE3P_Stokes/rms_a.m
+++ SE_Stokes_run/SE3P_Stokes/rms_a.m
@@ -9,43 +9,41 @@ opt.window = 'kaiser_exact';
 
 
 %% Direct solution
-if(exist('refval.mat'))
-	refval = load('refval.mat');
-	ref = refval.refv(:,1:3);
-else
-	ref = SE3P_Stokes_direct_fd_mex(1:N,x,f,ED_opt);
-end
 	%opt.window = 'gaussian';
 %ref = SE3P_Stokes(1:N,x,f,opt);
 %% Compare solutions with changing P
 str = {};
 M = 96; 
-opt.M = M*[L,L,L];
+
 PP = 4:2:32;
-rms_err = [];
+
 A = @(F,xi,L) F^(1/2)*(xi)^(1)*(L^(0));
-F = sum(norm(f).^2);
-a = A(F,opt.xi,L);
+
+
 for N = [10,50,100,200]
     for L = [1,2,3]
         opt.box = [L,L,L];
         opt.M = M*opt.box;
-        [x,f] = SE_charged_system(N,box,'vector');
+        [x,f] = SE_charged_system(N,opt.box,'vector');
         F = sum(norm(f).^2);
         for xi = 4:2:16
+	    rms_err = [];
+	    a = A(F,xi,L);
             opt.xi = xi;
             ED_opt.layers = (opt.M(1)-1)/2;
             ED_opt.xi = opt.xi;
-            ED_opt.box = box;
+            ED_opt.box = opt.box;
             ref = SE3P_Stokes_direct_fd_mex(1:N,x,f,ED_opt);
             for P = PP
                 opt.P = P;
                 u = SE3P_Stokes(1:N, x, f, opt);
                 rms_err = [rms_err rmse(u-ref)/a];
-                semilogy(PP,rms_err,'.b')
-                hold on
-            end
+	    end
+         semilogy(PP,rms_err,'.b')
+         hold on
+            
         end
+	exportgraphics(gcf,'rms_a.png')
     end
 end
 
