7d6af21e618da165cc7e49d6f37a3fab4bf7be62
ravemyr <ravemyr@kth.se>
2021-01-18 11:26:31 +0100

    imgs

--- SE_Stokes_run/SE3P_Stokes/P_error.m
+++ SE_Stokes_run/SE3P_Stokes/P_error.m
@@ -8,7 +8,7 @@ N = 10; % number of source particles
 
 
 %% Parameter selection
-L = 1; % box side length
+L = 2; % box side length
 box = [L L L]; % periodic box
 opt.box = box;
 
@@ -17,7 +17,7 @@ opt.box = box;
 
 M0 = 144; % Set M0=M/L, the restu * 1+ is automatic
 opt.M = M0*opt.box;
-opt.xi = 20;
+opt.xi = 10;
 opt.betaP = 2.5;
 opt.window = 'kaiser_exact';
 % charge-neutral system
