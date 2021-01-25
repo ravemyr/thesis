499e2d2134f25d36bb7d1eaadb97b7ac00deb854
ravemyr <ravemyr@kth.se>
2021-01-25 02:22:08 +0100

    imgs

--- SE_Stokes_run/SE3P_Stokes/xiLM_balance.m
+++ SE_Stokes_run/SE3P_Stokes/xiLM_balance.m
@@ -42,8 +42,8 @@ str = {};
 est = @(M,xi,L,f) sqrt(f)*(xi^3*L^2/(pi^4*(M/2)^(3/2)))*exp(-(pi*(M/2)/(xi*L))^2);
 e_vec = [];
 
-xx = [5,10];
-LL = [2,1];
+xx = [5,10,20];
+LL = [2,1,0.5];
 for xi = xx
     opt.xi = xi;
     for L = LL
