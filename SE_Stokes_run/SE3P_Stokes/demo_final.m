11563484218f0c5f23f99a4e2c3f06bed6a748f3
ravemyr <33254565+ravemyr@users.noreply.github.com>
2021-02-01 12:38:05 +0100

    first demo fixes

--- SE_Stokes_run/SE3P_Stokes/demo_final.m
+++ SE_Stokes_run/SE3P_Stokes/demo_final.m
@@ -1,14 +1,16 @@
 %demo final
 
 N = 10;
-[x, f] = SE_charged_system(N,box,'vector');
 L = 1;
+opt.box = [L,L,L];
+[x, f] = SE_charged_system(N,box,'vector');
+
 opt.xi = 20;
 tolerance = 10^(-10);
 opt.N = N;
 opt.x =x;
 opt.f = f;
-opt.box = [L,L,L];
+
 
 t = tic();
 opt = param_select_stokes(tolerance, opt);
@@ -16,3 +18,5 @@ t_par = toc(t);
 t = tic();
 u = SE3P_Stokes(1:N, x, f, opt);
 tSE = toc(t);
+disp('parameter selection time: %f \n',t_par)
+disp('computation time: ', tSE)
