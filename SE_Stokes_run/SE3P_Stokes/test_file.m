20e5865e4a54d6da8ce4172714e2ffca27d842c1
ravemyr <33254565+ravemyr@users.noreply.github.com>
2021-03-03 13:32:32 +0100

    added tracking to test function

--- SE_Stokes_run/SE3P_Stokes/test_file.m
+++ SE_Stokes_run/SE3P_Stokes/test_file.m
@@ -4,14 +4,19 @@ N_vals = [100,1000,10000];
 xi_vals = [15,20,30];
 L_vals = [1,2,4,5];
 tol_vals = [10^-6, 10^-8, 10^-10, 10^-12];
-
+r = [];
+tt = [];
 for n = N_vals
     for x = xi_vals
         for L = L_vals
             for tol = tol_vals
                 t = test_function(n,x,L,tol);	
                 assert(t(1)<tol,'N =%g, xi = %g, L = %f, tol = %g' ,n,x,L,tol)
+                r = [r, t(1)];
+                tt = [tt, t(3)];
             end
         end
     end
 end
+semilogy(tt,r,'*')
+exportgraphics(gcf,'testimg.png')
