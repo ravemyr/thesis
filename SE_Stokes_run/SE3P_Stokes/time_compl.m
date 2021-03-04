9e9e050068057de5271f74751618fc1e67f56886
ravemyr <ravemyr@kth.se>
2021-03-04 11:38:24 +0100

    imgs

--- SE_Stokes_run/SE3P_Stokes/time_compl.m
+++ SE_Stokes_run/SE3P_Stokes/time_compl.m
@@ -16,13 +16,13 @@ timings = [];
 
 for N = NN
     opt.N = N;
-    pt.box = 10^(1/3)*opt.box;
+    opt.box = 10^(1/3)*opt.box;
     [x, f] = SE_charged_system(N,opt.box,'vector');    
     opt.x =x;
     opt.f = f;
     opt = param_select_stokes(tolerance, opt);
     t = tic;
-    for it = 1:10
+    for it = 1:20
     	u = SE3P_Stokes(1:N, x, f, opt);
     end
     ti = toc(t);
