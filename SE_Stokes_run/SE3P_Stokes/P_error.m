dbd3b24d7337b7382aeec4cb93d8a69943bb6744
ravemyr <ravemyr@kth.se>
2021-03-18 12:37:29 +0100

    imgs

--- SE_Stokes_run/SE3P_Stokes/P_error.m
+++ SE_Stokes_run/SE3P_Stokes/P_error.m
@@ -8,7 +8,7 @@ N = 1000; % number of source particles
 
 
 %% Parameter selection
-L = 2; % box side length
+L = 1; % box side length
 box = [L L L]; % periodic box
 opt.box = box;
 
@@ -17,7 +17,7 @@ opt.box = box;
 
 M0 = 128; % Set M0=M/L, the restu * 1+ is automatic
 opt.M = M0*[1,1,1];
-opt.xi = 20;
+opt.xi = 5;
 opt.betaP = 2.5;
 opt.c = sqrt(0.91);
 %opt.window = 'kaiser_exact';
@@ -50,8 +50,10 @@ str = {};
 est_old = @(M,xi,L,f) sqrt(f)*(xi^3*L^2/(pi^4*(M/2)^(3/2)))*exp(-(pi*(M/2)/(xi*L))^2);
 %est = @(M,xi,L,f) sqrt(f)*(4/(3^(1/4)*L*pi))*exp(-(pi*M/(xi*L*2))^2);
 est = @(M,xi,L,F) 2*sqrt(F)*(xi*L)^2*exp(-(pi*(M/2)/(xi*L))^2)/(sqrt(3)*pi^2*L*(M/2));
+est_small = @(M,xi,L,F) 4*sqrt(F)/(sqrt(3)*pi*L)*exp(-(pi*M/(2*xi*L))^2);
 e_vec = [];
 ee_vec = [];
+eee_vec = [];
 F = sum(norm(f.^2));
 A = @(a,b,c) sqrt(a)*b*(b*c)^0;
 disp(num2str(A(F,opt.xi,opt.box(1))/rms_ref))
@@ -71,6 +73,8 @@ for M = [112 MM]
 	semilogy(PP,rms_err)
     ee = est_old(M,opt.xi,L,F);
     ee_vec = [ee_vec ee];
+    eee = est_small(M,opt.xi,L,F);
+    eee_vec = [eee_vec eee];
 	hold on
 	str = [str strcat('M = ',num2str(M))];
 end
@@ -85,6 +89,7 @@ end
 for i = 1:length(e_vec)
     semilogy(PP,ones(1,length(PP))*e_vec(i),('bl--'))
     semilogy(PP,ones(1,length(PP))*ee_vec(i),'r--')
+    semilogy(PP,ones(1,length(PP))*eee_vec(i),'g--')
 end
 str = [str,'estimate'];
 opt
