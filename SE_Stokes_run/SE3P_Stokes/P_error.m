df1c17edea76e1a7d9483e8408fc7b34d49aa9b4
ravemyr <ravemyr@kth.se>
2021-04-26 17:51:39 +0200

    img

--- SE_Stokes_run/SE3P_Stokes/P_error.m
+++ SE_Stokes_run/SE3P_Stokes/P_error.m
@@ -102,6 +102,7 @@ str = [str,'estimate'];
 opt
 legend(str,'FontSize',14,'Location','SouthWest')
 xlabel('P','FontSize',14)
+ylabel('Absolute error', 'FontSize',14)
 xlim([1,32])
 ylim([10^-14,1])
 grid on
